46f781b5539706e5fef4da51109b8c1d2000383c
Saicharan Gouru <gourusaicharan6@gmail.com>
2021-08-21 19:49:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-.container {
-    display: flex;
-    flex - direcion: column;
-}
+var billAmount = document.querySelector("#bill-amount");
+var amountGiven = document.querySelector("#amount-given");
+var btnCheck = document.querySelector("#btn-check");
+
+btnCheck.addEventListener("click", function a() { alert(billAmount.value + amountGiven.value) });
