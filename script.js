dc23ee9ec7ccdcc74b26046ccaa3e146ae3a336f
Saicharan Gouru <gourusaicharan6@gmail.com>
2021-08-27 18:31:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,13 +2,15 @@ var billAmount = document.querySelector("#bill-amount");
 var amountGiven = document.querySelector("#amount-given");
 var btnCheck = document.querySelector("#btn-check");
 var message = document.querySelector("#error-message");
+var availableNotes = [2000, 500, 100, 50, 20, 10, 5, 2, 1];
+var noOfNotes = document.querySelectorAll(".no-of-notes");
 
 btnCheck.addEventListener("click", function a() {
     message.style.display = "none";
     if (Number(billAmount.value) > 0) {
         if (Number(amountGiven.value) >= Number(billAmount.value)) {
-
-
+            const cashToBeReturned = Number(amountGiven.value) - Number(billAmount.value);
+            calculateChange(cashToBeReturned);
         } else {
             displayErrorMessage("Amount Given Should Be Atleast Equal To Bill Amount");
         }
@@ -20,4 +22,12 @@ btnCheck.addEventListener("click", function a() {
 function displayErrorMessage(Emsg) {
     message.style.display = "block";
     message.innerText = Emsg;
+}
+
+function calculateChange(cashToBeReturned) {
+    for (let i = 0; i < availableNotes.length; i++) {
+        const numberOfNotes = Math.trunc(cashToBeReturned / availableNotes[i]);
+        cashToBeReturned %= availableNotes[i];
+        noOfNotes[i].innerText = numberOfNotes;
+    }
 }
